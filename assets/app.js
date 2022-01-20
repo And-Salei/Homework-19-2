3efb63cb00a6aace85aee8ea14399dc6ee1c1329
And-Salei <saleiandrew@gmail.com>
2022-01-20 15:37:50 +0200

    Ok!

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 let Year = +prompt ('Введите Год');
 
-if ((Year % 400 == 0) || (Year % 4 == 0) || (Year % 100 != 0)) {console.log('Год Високосный');}
+if ((Year % 400 == 0) || (Year % 4 == 0) && (Year % 100 != 0)) {console.log('Год Високосный');}
+
 else {console.log('Год Обычный');}
 
 console.log(`${Year} год`);
